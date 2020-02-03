cc5f5d81f4d1b8c59a618829fad09bd9d7ae81c4
Mark <marcus.koh16.01.97@gmail.com>
2020-02-03 16:10:43 +0300

    a bit refactor

--- vidv.py
+++ vidv.py
@@ -1,10 +1,10 @@
-import aiohttp
-import asyncio
 import random
-import sys
 import argparse
 import re
 import time
+import asyncio
+import aiohttp
+
 
 CONFIG = {
     'oid': 0,
@@ -14,30 +14,42 @@ CONFIG = {
 
 
 class VideoPayload:
+    """
+        Parse json file and get info from video
+    """
     def __init__(self, payload_object):
         self.payload = payload_object['payload'][1]
-        self.videoData = self.payload[4]
-        self.videoInfo = self.videoData['mvData']
-        self.playerParams = self.videoData['player']['params'][0]
+        self.video_data = self.payload[4]
+        self.video_info = self.video_data['mvData']
+        self.player_params = self.video_data['player']['params'][0]
 
 
 class CheatVideoViews:
+    """
+    CheatVideoViews class with asyncs funcs
+    """
     def __init__(self):
-        self.requestCount = 0
-        self.startTime = time.time()
+        self.requests_count = 0
+        self.start_time = time.time()
 
     @staticmethod
     async def video_params():
+        """
+            Get video params
+            video data and player params
+        """
         try:
             async with aiohttp.ClientSession() as session:
-
                 async with session.post("https://vk.com/al_video.php", data={
-                    'al': 1,
-                    'act': "show",
-                    'video': f'{CONFIG["oid"]}_{CONFIG["vid"]}'
+                        'al': 1,
+                        'act': "show",
+                        'video': f'{CONFIG["oid"]}_{CONFIG["vid"]}'
                 }, headers={
                     "cookie": '',
-                    "user-agent": f'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/{(random.random() * 600)}.36 (KHTML, like Gecko) Chrome/{(random.random() * 59)}.0.3029.110 Safari/${(random.random() * 1000)}.36',
+                    "user-agent": f'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/'
+                                  f'{(random.random() * 600)}.36 (KHTML, like Gecko) Chrome/'
+                                  f'{(random.random() * 59)}.0.3029.110 Safari/$'
+                                  f'{(random.random() * 1000)}.36',
                     "sec-fetch-mode": "cors",
                     "referer": "https://vk.com",
                     "X-Requested-With": "XMLHttpRequest",
@@ -47,48 +59,65 @@ class CheatVideoViews:
 
                     vid = VideoPayload(json)
             return vid
-        except Exception as e:
-            print(e)
+        except (RuntimeError, ConnectionError) as error:
+            print(error)
 
     @staticmethod
-    async def request_increment_view_count(hash):
+    async def request_increment_view_count(hash_date):
+        """
+            POST increase video view count
+        """
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.post("https://vk.com/al_video.php", data={
-                    'act': "video_view_started",
-                    'al': 1,
-                    'oid': str(CONFIG['oid']),
-                    'vid': str(CONFIG['vid']),
-                    'hash': hash
+                        'act': "video_view_started",
+                        'al': 1,
+                        'oid': str(CONFIG['oid']),
+                        'vid': str(CONFIG['vid']),
+                        'hash': hash_date
                 }, headers={
                     "cookie": '',
-                    "user-agent": f'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/{(random.random() * 600)}.36 (KHTML, like Gecko) Chrome/{(random.random() * 59)}.0.3029.110 Safari/${(random.random() * 1000)}.36',
+                    "user-agent": f'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/'
+                                  f'{(random.random() * 600)}.36 (KHTML, like Gecko) Chrome/'
+                                  f'{(random.random() * 59)}.0.3029.110 Safari/$'
+                                  f'{(random.random() * 1000)}.36',
                     "sec-fetch-mode": "cors",
                     "referer": "https://vk.com",
                     "X-Requested-With": "XMLHttpRequest",
                     "content-type": "application/x-www-form-urlencoded"
                 }) as response:
                     return response
-        except Exception as e:
-            print(e)
+        except (RuntimeError, ConnectionError) as error:
+            print(error)
 
-    async def start(self, i=None):
+    async def start(self):
+        """
+            Start and print info status
+        """
         while True:
             video_payload = await self.video_params()
-            await self.request_increment_view_count(hash=video_payload.playerParams['view_hash'])
+            await self.request_increment_view_count(hash_date=video_payload.player_params['view_hash'])
             print(
-                f"Views counts: {video_payload.videoInfo['info'][10]}. Time from start: {int(time.time() - self.startTime)}  Requests counts: {self.requestCount}")
-            self.requestCount = self.requestCount + 1
+                f"Views counts: {video_payload.video_info['info'][10]}. Time from start: "
+                f"{int(time.time() - self.start_time)}  Requests counts: "
+                f"{self.requests_count}")
+            self.requests_count = self.requests_count + 1
             await asyncio.sleep(random.randrange(0, 1))
 
 
 async def asynchronous():
+    """
+        Init async
+    """
     script = CheatVideoViews()
-    tasks = [asyncio.ensure_future(script.start(i)) for i in range(0, int(CONFIG['threads']))]
+    tasks = [asyncio.ensure_future(script.start()) for i in range(0, int(CONFIG['threads']))]
     await asyncio.wait(tasks)
 
 
 def main():
+    """
+        Just main/ Init loop
+    """
     ioloop = asyncio.get_event_loop()
     print('Asynchronous:')
     ioloop.run_until_complete(asynchronous())
@@ -96,36 +125,40 @@ def main():
 
 
 def get_uid_vid(url):
+    """
+        Get uid and vid data
+    """
     try:
         match = re.findall('video-?(\\d+)_(\\d+)', str(url))[0]
         CONFIG['oid'] = match[0]
         CONFIG['vid'] = match[1]
-    except IndexError as e:
+    except IndexError as error:
         print('Invalid input')
-        raise e
+        raise error
 
 
 def get_threads(threads):
+    """
+        Threads count
+    """
     CONFIG['threads'] = threads
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='VidViews')
-    parser.add_argument('-t', '--threads', type=int, help='Threads count\n -t 50')
-    parser.add_argument('-u', '--url', type=str, help='Url\n -u https://videos242888501?z=video242888501_456239030')
+    PARSER = argparse.ArgumentParser(description='VidViews')
+    PARSER.add_argument('-t', '--threads', type=int, help='Threads count\n -t 50')
+    PARSER.add_argument('-u', '--url', type=str, help='Url\n -u https://videos242888501?z=video242888501_456239030')
 
-    args = parser.parse_args()
-
+    ARGS = ARGS.parse_args()
 
     try:
-        if args.threads:
-            get_threads(args.threads)
-        get_uid_vid(args.url)
+        if ARGS.threads:
+            get_threads(ARGS.threads)
+        get_uid_vid(ARGS.url)
         main()
-    except Exception as e:
+    except Exception as error:
         print('URL is required.\n')
-        print(e)
-    print('\nInput example: vidv.exe -u https://videos242888501?z=video242888501_456239030 -t 100\nOr: vidvk.exe -u video242888501_456239030 -t 50')
-
-   
+        print(error)
+    print('\nInput example: vidv.exe -u https://videos242888501?z=video242888501_456239030 -t 100\nOr: vidvk.exe -u '
+          'video242888501_456239030 -t 50')
